6eaa0f33aeb4c31ab66b65a4386a523500a934df
fathiaqurrata <fathiaqurrataaini@gmail.com>
2023-11-01 14:52:48 +0700

    Initial commit

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Oops! Something went wrong.</div>
+            <p>{{ $error }}</p>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
